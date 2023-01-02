57bbeef3a92886b50af1bacb1a1bef4682341535
Ponhrith <ponhrith.nhep@gmail.com>
2023-01-02 10:05:18 +0700

    Testing

--- Week2/Ex1.cpp
+++ Week2/Ex1.cpp
@@ -1,15 +1,38 @@
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int numOne, numTwo, max_two;
+//     cout<<"Enter the Two Numbers: ";
+//     cin>>numOne>>numTwo;
+//     if(numOne>numTwo)
+//         max_two = numOne;
+//     else
+//        max_two = numTwo;
+//     cout<<"\nMaximum = "<<max_two;
+//     cout<<endl;
+//     return 0;
+// }
+
 #include<iostream>
 using namespace std;
-int main()
-{
-    int numOne, numTwo, max_two;
-    cout<<"Enter the Two Numbers: ";
-    cin>>numOne>>numTwo;
-    if(numOne>numTwo)
-        max_two = numOne;
-    else
-       max_two = numTwo;
-    cout<<"\nMaximum = "<<max_two;
-    cout<<endl;
+
+int max_two(int x, int y){
+
+    if(x > y){
+        return x;
+    }
+    else{
+        return y;
+    }
+}
+
+int main(){
+    int x, y;
+    
+    cout << "Enter 2 intergers: ";
+    cin >> x >> y;
+
+    cout << "The biggest number is: " << max_two(x, y);
     return 0;
 }
