63ade2c14013bdc029db988fb2d3ec9b0fbc413f
Ponhrith <ponhrith.nhep@gmail.com>
2023-01-02 11:13:49 +0700

    Testing

--- Week2/Ex5.cpp
+++ Week2/Ex5.cpp
@@ -5,8 +5,34 @@ class informRUPP{
 private:
     int numFaculty;
     string nameFaculty;
-    string feDepartment;
+    string feDepartment; 
 
 public:
+    informRUPP(int num = 1, string feName ="Faculty of Engineering", string nameDep = "ITE" ){
+        numFaculty = num;
+        nameFaculty = feName;
+        feDepartment = nameDep;
+    }
+
+    int getNum(){
+        return numFaculty;
+    }
+
+    string getFacName(){
+        return nameFaculty;
+    }
     
+    string getNameDep(){
+        return feDepartment;
+    }
 }; 
+
+// class feDepartment : informRUPP{
+// private:
+//     string nameDepartment;
+// public:
+//     feDepartment(string nameDep = "ITE"){
+//         nameDepartment = nameDep;
+//     }
+
+// };
