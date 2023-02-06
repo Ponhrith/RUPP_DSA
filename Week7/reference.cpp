84a4729744059908c65d9e975ab84a13cde95555
Ponhrith <ponhrith.nhep@gmail.com>
2023-02-06 10:13:47 +0700

    Testing

--- Week7/reference.cpp
+++ Week7/reference.cpp
@@ -1,18 +1,41 @@
 #include <iostream>
 using namespace std;
 
+
+void display_array(int ar[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout << ar[i] << " ";
+    }
+    cout << endl;
+}
+
+void insert_data(int arr[], int newData, int pos)
+{
+    int i;
+    int n = sizeof(arr)/sizeof(arr[0]);
+    for (i=n; i>pos; i--)
+    {
+        arr[i] = arr[i-1];
+    }
+    arr[i] = newData;
+    n++;
+}
+
 int main(){
-    int number = 88;
-    int & refNumber = number;
+    system("cls");
+    int my_arr [] = {3,2,6,4,7};
+    int arr_size = sizeof(my_arr)/sizeof(my_arr[0]);
+    cout<<"The current element in my array is: ";
+    display_array(my_arr, arr_size);
 
-    cout << number << endl;
-    cout << refNumber << endl;
+    int newEle = 10;
+    int pos = 3;
+    insert_data(my_arr, newEle, pos);
 
-    refNumber = 99;
-    cout << refNumber << endl;
-    cout << number << endl;
-
-    number = 55;
-    cout << number << endl;
-    cout << refNumber << endl;
+    cout<<"The current element after add"<< newEle <<"to position"<< pos <<" :";
+    display_array(my_arr, arr_size);
+    cout<<endl;
+    return 0;
 }
