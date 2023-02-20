35b34bcf1ec7e377221ec314168429a311d0eacc
Ponhrith <ponhrith.nhep@gmail.com>
2023-02-20 11:26:25 +0700

    Testing

--- Lecture8/linkedList.cpp
+++ Lecture8/linkedList.cpp
@@ -1,22 +1,29 @@
-#include <iostream>
-#include <stdlib.h>
+#include<iostream>
 using namespace std;
 
-struct node{
-    int data,data1;
+struct node
+{
+    int data;
     struct node *link;
 };
-
 int main(){
+
     struct node *head = NULL;
     struct node *tail = NULL;
     head = (struct node *)malloc(sizeof(struct node));
     tail = (struct node *)malloc(sizeof(struct node));
+
     head->data = 8;
     head->link = tail;
-    tail->data1 = 15;
+
+    tail->data = 15;
     tail->link = NULL;
-    cout << head->data << endl;
-    cout << tail->data1;
+
+    while(head != NULL)
+    {
+        cout << head->data << endl;
+        head = head->link;
+    }
+    
     return 0;
 }
