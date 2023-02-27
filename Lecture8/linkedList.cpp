20c24e0a8d76e6e5295c7ca81a965f0e0dc6e2eb
Ponhrith <ponhrith.nhep@gmail.com>
2023-02-27 09:08:53 +0700

    Done

--- Lecture8/linkedList.cpp
+++ Lecture8/linkedList.cpp
@@ -9,18 +9,33 @@ struct node
 int main(){
 
     struct node *head = NULL;
+    struct node *second = NULL;
+    struct node *third = NULL;
+    struct node *fourth = NULL;
     struct node *tail = NULL;
     head = (struct node *)malloc(sizeof(struct node));
+    second = (struct node *)malloc(sizeof(struct node));
+    third = (struct node *)malloc(sizeof(struct node));
+    fourth = (struct node *)malloc(sizeof(struct node));
     tail = (struct node *)malloc(sizeof(struct node));
 
-    head->data = 8;
-    head->link = tail;
+    head->data = 35;
+    head->link = second;
 
-    tail->data = 15;
+    second->data = 43;
+    second->link = third;
+
+    third->data = 19;
+    third->link = fourth;
+
+    fourth->data = 65;
+    fourth->link = tail;
+
+    tail->data = 38;
     tail->link = NULL;
 
     while(head != NULL)
-    {
+    { 
         cout << head->data << endl;
         head = head->link;
     }
