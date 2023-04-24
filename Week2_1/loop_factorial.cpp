d1e60a30fa090397cf8cb1df86fdf10538179fd5
Ponhrith <ponhrith.nhep@gmail.com>
2023-04-24 10:53:52 +0700

    Recursive Function

--- Week2_1/loop_factorial.cpp
+++ Week2_1/loop_factorial.cpp
@@ -1,19 +1,30 @@
 #include<iostream>
 using namespace std;
 
+
+int recurfact(int dum)
+{
+    if (dum == 0)
+        return 1;
+    else
+        return dum*recurfact(dum-1);
+    
+}
+
 int fact(int num)
 {
     int sum = 1;
     for (int i = 1; i <= num; ++i) {
         sum *= i;
     }
-    cout << "Factorial of " << num << " is " << sum << std::endl;
+    cout << "Loop Factorial " << num << " is " << sum << std::endl;
     return sum;
 }
 
 
-
 int main() {
+    cout << "Recursive Factorial: " << recurfact(5) << std::endl; 
     fact(5);
+
     
 }
