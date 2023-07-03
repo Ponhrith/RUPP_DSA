78357f8cdd6684f491b485519f13462d827c14f0
Ponhrith <ponhrith.nhep@gmail.com>
2023-07-03 11:06:37 +0700

    Testing

---
+++ Midterm/Nhep Ponhrith-2. Quadratic Probing Hash-Table.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <list>
+
+using namespace std;
+
+class QuadraticProbingHashTable {
+private:
+    list<int>* table;
+    int tableSize;
+
+public:
+    QuadraticProbingHashTable(int size) {
+        tableSize = size;
+        table = new list<int>[tableSize];
+    }
+
+    int hashFunction(int key) {
+        return key % tableSize;
+    }
+
+    void insertKey(int key) {
+        int index = hashFunction(key);
+        int i = 1;
+
+        while (!table[index].empty()) {
+            index = (index + i * i) % tableSize;
+            i++;
+        }
+
+        table[index].push_back(key);
+    }
+
+    void displayTable() {
+        for (int i = 0; i < tableSize; i++) {
+            cout << i << " -> ";
+            if (!table[i].empty()) {
+                for (int key : table[i]) {
+                    cout << key << " ";
+                }
+            }
+            cout << endl;
+        }
+    }
+};
+
+int main() {
+    int tableSize = 10;
+
+    QuadraticProbingHashTable hashTable(tableSize);
+
+    // Create a list of keys
+    list<int> keys = {4371, 1323, 6173, 4199, 4344, 9679, 1989};
+
+    // Insert keys into the hash table
+    for (int key : keys) {
+        hashTable.insertKey(key);
+    }
+
+    cout << "Hash Table:" << endl;
+    hashTable.displayTable();
+
+    return 0;
+}
